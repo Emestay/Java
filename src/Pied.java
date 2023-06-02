942eac59e21e3395cbf4b236f6eb633d01368224
Emestay <103575914+Emestay@users.noreply.github.com>
2023-06-02 11:18:46 +0200

    init java

---
+++ src/Pied.java
@@ -0,0 +1,10 @@
+public class Pied {
+    public static void main(String[] args) {
+        int f = 10, l = 5, o = 3;
+        System.out.println(soustraction(f ,l , o));
+    }
+    public static int soustraction(int f, int l, int o) {
+        return f - l - o;
+    }
+
+}
